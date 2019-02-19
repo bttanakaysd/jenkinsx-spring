664d772bd7b870f8f3bfc1b7676190f8c5fd6a56
? <?>
2019-02-19 11:57:15 +0900

    add hello page.

--- src/main/java/com/example/jenkinsxspring/DemoApplication.java
+++ src/main/java/com/example/jenkinsxspring/DemoApplication.java
@@ -2,10 +2,19 @@ package com.example.jenkinsxspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class DemoApplication {
-
+	
+	@RequestMapping("/")
+	public String hello() {
+		System.out.println("hello!");
+		return "hello";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
